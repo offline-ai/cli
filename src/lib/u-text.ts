525db76fe2ba9ad7bb1084e31294066116cc2719
Riceball LEE <snowyu.lee@gmail.com>
2024-05-28 13:12:30 +0800

    feat(uText): using random font if no specify font

--- src/lib/u-text.ts
+++ src/lib/u-text.ts
@@ -1,15 +1,34 @@
 import colors from 'ansi-colors'
 import figlet from 'figlet'
-import type { Fonts } from 'figlet';
 
 const textSync = figlet.textSync
 
-export function uText(s: string, aColor = 'gray', font: Fonts = '3D-ASCII') {
-  return colors[aColor as any](
-    textSync(s, {
-      font,
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
-    })
+const excludeFonts = new Set([
+  'Gradient', 'Hex', 'Trek', 'Term', 'Runic', 'Runyc', 'Rot13', 'Octal', 'Mshebrew210', 'Mike', '1Row', 'DWhistled',
+  'Tengwar', 'Small Tengwar',
+])
+
+const fonts = figlet.fontsSync().filter((f: string) => !excludeFonts.has(f))
+
+function getRandomInt(size: number) {
+  return Math.floor(Math.random() * size);
+}
+
+export function uText(s: string, options: any= {}) {
+  const color = options.color || 'gray'
+  if (!options.font) {
+    // '3D-ASCII'
+    options.font = fonts[getRandomInt(fonts.length)]
+  }
+      // horizontalLayout: 'default',
+      // verticalLayout: 'default',
+  return colors[color as any](
+    textSync(s, options)
   );
 }
+
+// import cfonts from 'cfonts'
+
+// export function uText(s: string, options: any = {}) {
+//   cfonts.say(s, options)
+// }
