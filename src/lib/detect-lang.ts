996308e21c4fb78e550eb6755870033dbcb64409
Riceball LEE <snowyu.lee@gmail.com>
2024-05-29 21:35:22 +0800

    fix: can detect lang for 2 chars now

--- src/lib/detect-lang.ts
+++ src/lib/detect-lang.ts
@@ -2,7 +2,7 @@ import { franc } from 'franc-all'
 import {iso6393} from 'iso-639-3'
 
 export function detectLang(text: string, options: any= {}): string|undefined {
-  if (options.minLength === undefined) { options.minLength = 6 }
+  if (options.minLength === undefined) { options.minLength = 2 }
   const isoCode = franc(text, options)
   let result: any = isoCode !== 'und' ? iso6393.find(i => i.iso6393 === isoCode) : undefined
   if (result) {result = result.name}
