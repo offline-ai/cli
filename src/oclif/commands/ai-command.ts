3c1ad5f989629abcc95d1264854af822ec9212e2
Riceball LEE <snowyu.lee@gmail.com>
2024-06-01 16:38:27 +0800

    feat(AICommand): add loadConfig method to AICommand

--- src/oclif/commands/ai-command.ts
+++ src/oclif/commands/ai-command.ts
@@ -1,10 +1,26 @@
+import path from 'path'
 import {Command, Flags} from '@oclif/core'
+import { loadAIConfig, loadConfigFile } from '../../lib/load-config.js'
+import { defaultsDeep } from 'lodash-es'
 
 // const CONFIG_BASE_NAME = '.ai'
 
 export abstract class AICommand extends Command {
 
   static flags : Record<string, any> = {
-    config: Flags.directory({char: 'c', description: 'the config directory', exists: true})
+    config: Flags.file({char: 'c', description: 'the config file', exists: true})
+  }
+
+  loadConfig(configFile?: string) {
+    let result = loadAIConfig(this.config)
+    if (configFile) {
+      configFile = path.resolve(configFile)
+      const config = loadConfigFile(configFile)
+      if (!config) {
+        this.error(`config file ${configFile} not found`)
+      }
+      result = defaultsDeep(config, result)
+    }
+    return result
   }
 }
