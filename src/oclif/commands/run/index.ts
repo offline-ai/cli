1b8c22874d49745f35f3b002090a4c2e4f03a8cb
Riceball LEE <snowyu.lee@gmail.com>
2024-06-01 19:24:02 +0800

    refactor: move abstract ai command to lib folder

--- src/oclif/commands/run/index.ts
+++ src/oclif/commands/run/index.ts
@@ -3,15 +3,13 @@ import {Args, Flags} from '@oclif/core'
 import { parseJsJson } from '@isdk/ai-tool'
 import { LogLevelMap, logLevel } from '@isdk/ai-tool-agent'
 
-import { AICommand } from '../ai-command.js'
+import { AICommand } from '../../lib/ai-command.js'
 import {runScript} from '../../../lib/run-script.js'
 import { showBanner } from '../../lib/help.js'
 import { loadConfigFile } from '../../../lib/load-config.js'
 import { defaultsDeep } from 'lodash-es'
 
 export default class RunScript extends AICommand {
-  static enableJsonFlag = true
-
   static args = {
     data: Args.string({
       description: 'the json data which will be passed to the ai-agent script',
@@ -24,7 +22,7 @@ export default class RunScript extends AICommand {
   static description = 'Execute ai-agent script file and return result.'
 
   static examples = [
-    `<%= config.bin %> <%= command.id %> ./script.yaml "{content: 'hello world'}"
+    `<%= config.bin %> <%= command.id %> -f ./script.yaml "{content: 'hello world'}"
 ┌────────────────────
 │[info]:Start Script: ...
 `,
@@ -33,11 +31,11 @@ export default class RunScript extends AICommand {
   static flags = {
     ...AICommand.flags,
     api: Flags.url({char: 'u', description: 'the api URL'}),
-    searchPaths: Flags.directory({char: 'p', description: 'the search paths for ai-agent script file', exists: true, multiple: true}),
+    searchPaths: Flags.directory({char: 's', description: 'the search paths for ai-agent script file', exists: true, multiple: true}),
     logLevel: Flags.string({char: 'l', description: 'the log level', options: ['silence', 'fatal', 'error', 'warn', 'info', 'debug', 'trace']}),
     interactive: Flags.boolean({char: 'i', description: 'interactive mode', allowNo: true}),
     history: Flags.file({char: 'h', description: 'the chat history file for interactive mode to record', dependsOn: ['interactive']}),
-    stream: Flags.boolean({char: 's', description: 'stream mode', allowNo: true}),
+    stream: Flags.boolean({char: 'm', description: 'stream mode, defaults to true', allowNo: true}),
     banner: Flags.boolean({char: 'b', description: 'show banner', allowNo: true}),
     script: Flags.string({char: 'f', description: 'the ai-agent script file name or id'}),
     dataFile: Flags.file({char: 'd', description: 'the data file which will be passed to the ai-agent script'}),
