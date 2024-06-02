f9f56ae4af16d36929832112258b749dbed58379
Riceball LEE <snowyu.lee@gmail.com>
2024-06-02 21:37:45 +0800

    feat: add saveConfigFile func

--- src/lib/load-config.ts
+++ src/lib/load-config.ts
@@ -1,10 +1,12 @@
-const DEFAULT_CONFIG_NAME = '.ai'
+export const DEFAULT_CONFIG_NAME = '.ai'
 
-import { expandObjEnv } from "@isdk/ai-tool";
-import { ConfigFile } from "@isdk/ai-tool-prompt"
-import { defaultsDeep } from "lodash-es"
+import { expandObjEnv, getMultiLevelExtname } from "@isdk/ai-tool";
+import { ConfigFile, stringifyYaml } from "@isdk/ai-tool-prompt"
+import { defaultsDeep, omit } from "lodash-es"
+import fs from 'fs'
 import path from 'path'
 import type { Config } from "@oclif/core";
+import {mimeType} from 'mime-type/with-db'
 
 export function loadConfigFile(filename: string, searchPaths: string[] = ['.']) {
   if (path.isAbsolute(filename)) {return ConfigFile.loadSync(filename)}
@@ -32,10 +34,12 @@ export function loadConfig(filename: string, config: Config) {
       brainDir: [path.join(config.dataDir, 'brain')],
       agentDirs: [path.join(config.dataDir, 'agent'), '$PWD'],
       promptDirs: [path.join(config.dataDir, 'prompt')],
+      chatsDir: path.join(config.dataDir, 'log', 'chats'),
+      inputsDir: path.join(config.dataDir, 'log', 'inputs'),
     }
   }
-
-  for (const [key, value] of Object.entries(getXDGConfigs(config))) {
+  const XDGConfigs = getXDGConfigs(config)
+  for (const [key, value] of Object.entries(XDGConfigs)) {
     defaultConfig[key] = value
   }
 
@@ -44,7 +48,8 @@ export function loadConfig(filename: string, config: Config) {
   if (defaultConfig.AI_CONFIG_BASENAME) {
     filename = defaultConfig.AI_CONFIG_BASENAME
   }
-  return expandConfig(loadConfigFile(filename, searchPaths), defaultConfig)
+  const result = expandConfig(loadConfigFile(filename, searchPaths), defaultConfig)
+  return defaultsDeep(result, omit(defaultConfig, Object.keys(XDGConfigs)))
 }
 
 export function loadAIConfig(config: Config) {
@@ -59,4 +64,23 @@ export function getXDGConfigs(config: Config) {
     XDG_BIN_HOME: path.dirname(config.options.root),
   }
   return result
+}
+
+export function saveConfigFile(filename: string, config: any, extLevel = 1) {
+  if (filename[0] === '.') {extLevel++}
+  const extname = getMultiLevelExtname(filename, extLevel)
+  if (!extname || (extname.split('.').length <= 1)) {filename += '.yaml'}
+  const mime = mimeType.lookup(filename) as string
+  if (mime === 'application/json')
+    config = JSON.stringify(config, null, 2)
+  else if (mime === 'text/yaml') {
+    config = stringifyYaml(config)
+  } else {
+    throw new Error(`${filename} unsupported mime type: ${mime}`)
+  }
+  const dirname = path.dirname(filename)
+  if (!fs.existsSync(dirname)) {
+    fs.mkdirSync(dirname, {recursive: true})
+  }
+  fs.writeFileSync(filename, config, {encoding: 'utf8'})
 }
