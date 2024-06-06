4b36b09ac62494c2a271e4b61f7f11dd1b25e885
Riceball LEE <snowyu.lee@gmail.com>
2024-06-06 08:16:15 +0800

    build: upgrade ocli to v4

--- bin/dev.js
+++ bin/dev.js
@@ -1,5 +1,6 @@
-#!/usr/bin/env -S node --import=./bin/register.js --no-warnings=ExperimentalWarning
+#!/usr/bin/env -S npx tsx
 // #!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
+// #!/usr/bin/env -S node --import=./bin/register.js --no-warnings=ExperimentalWarning
 // #!/usr/bin/env bun
 
 import {execute} from '@oclif/core'
