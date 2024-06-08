61d1024bc011b75cafa7902f72f9d75af8513515
Riceball LEE <snowyu.lee@gmail.com>
2024-06-08 21:23:50 +0800

    feat(config): can get single config item value now

--- src/oclif/commands/config/index.ts
+++ src/oclif/commands/config/index.ts
@@ -1,13 +1,34 @@
+import { Args } from '@oclif/core'
 import { getXDGConfigs } from '../../../lib/load-config.js'
 import { AICommand } from '../../lib/ai-command.js'
 import { showBanner } from '../../lib/help.js'
+import { get as getByPath } from 'lodash-es'
 
 export default class AIConfigCommand extends AICommand {
   static enableJsonFlag = true
 
+  static args = {
+    item_name: Args.string({
+      description: 'the config item name path to get',
+    })
+  }
+
   static summary = '🛠️  Manage the AI Configuration.'
 
   static description = 'show current configuration if no commands.'
+
+  static examples = [`
+# list all configurations
+<%= config.bin %> <%= command.id %>`,
+`# get the brainDir config item
+<%= config.bin %> <%= command.id %> brainDir
+
+AI Configuration:
+{
+  "brainDir": "~/.local/share/ai/brain"
+}
+`
+  ]
 
   static flags = {
     ...AICommand.flags,
@@ -15,7 +36,7 @@ export default class AIConfigCommand extends AICommand {
 
   async run(): Promise<any> {
     const opts = await this.parse(AIConfigCommand)
-    const {flags} = opts
+    const {args, flags} = opts
     const isJson = this.jsonEnabled()
     const userConfig = this.loadConfig(flags.config, opts)
     const hasBanner = flags.banner ?? userConfig.banner ?? true
@@ -24,7 +45,17 @@ export default class AIConfigCommand extends AICommand {
     this.logJson(getXDGConfigs(this.config))
     if (userConfig) {
       this.log('\nAI Configuration:')
-      this.logJson(userConfig)
+      if (args.item_name) {
+        const v = getByPath(userConfig, args.item_name)
+        if (v === undefined) {
+          this.logJson(userConfig)
+          this.error(`config item "${args.item_name}" not found`)
+        }
+        this.logJson({[args.item_name]: v})
+      }
+      else {
+        this.logJson(userConfig)
+      }
     }
   }
 }
