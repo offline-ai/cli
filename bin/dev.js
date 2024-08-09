9ac06306ee9f2ecf645ff5bc0e591ace5d5be48f
Riceball LEE <snowyu.lee@gmail.com>
2024-08-09 12:58:50 +0800

    feat: add --trace-warnings for develop

--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S npx tsx
+#!/usr/bin/env -S npx --node-options='--trace-warnings' tsx
 // #!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
 // #!/usr/bin/env -S node --import=./bin/register.js --no-warnings=ExperimentalWarning
 // #!/usr/bin/env bun
