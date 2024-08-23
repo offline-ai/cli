b12a4d4cf56aa8a6e49ada10f44a07266c3cb4e1
Riceball LEE <snowyu.lee@gmail.com>
2024-08-23 18:58:54 +0800

    feat: add AIHelp init

--- src/oclif/lib/help.ts
+++ src/oclif/lib/help.ts
@@ -1,6 +1,14 @@
-// import { type Command, Help } from '@oclif/core';
+import { type Command } from '@oclif/core';
 import { CustomHelp } from '@offline-ai/cli-common'
 
 export { showBanner } from '@offline-ai/cli-common'
 
-export default CustomHelp
+export default class AIHelp extends CustomHelp {
+  async showHelp(args: string[]) {
+    super.showHelp(args);
+  }
+
+  async showCommandHelp(command: Command.Loadable) {
+    super.showCommandHelp(command);
+  }
+}
