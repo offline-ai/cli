c52e45d4727a416e5c777f77144766e680df904c
Riceball LEE <snowyu.lee@gmail.com>
2024-08-25 07:07:25 +0800

    chore: minor change

--- src/lib/global-fetch-proxy.ts
+++ src/lib/global-fetch-proxy.ts
@@ -1,8 +1,17 @@
+// import { Agent } from 'undici'
 import { createProxy } from "node-fetch-native/proxy";
 
 const fetch = globalThis.fetch
-globalThis.fetch = function (input: RequestInfo | URL, init?: RequestInit | undefined) {
+globalThis.fetch = function (input: RequestInfo | URL, init?: RequestInit | undefined | any) {
   if (!init) {init = {}}
-  init = {...createProxy(), ...init}
+  init = {
+    ...createProxy(),
+    ...init,
+    // dispatcher: new Agent({
+    //   connect: { timeout: 600000 },
+    //   // keepAliveTimeout: 20000,
+    //   // keepAliveMaxTimeout: 20000,
+    // }),
+  }
   return fetch(input, init)
 }
